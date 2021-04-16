2f9ae74d2a7370560a2ac357e5fda4f3edcd4693
Wes Howard <BigRonald42@hotmail.com>
2021-04-16 08:13:11 -0700

    changed name of DB in mongo

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,7 +3,7 @@ const router = require("express").Router()
 
 
 //GET  /api/workouts get all workouts
-router.get("/workouts",async function(req,res){  
+router.get("/workouts", async function (req, res) {
     const data = await Workout.aggregate([
         {
             $addFields: {
@@ -14,35 +14,47 @@ router.get("/workouts",async function(req,res){
         }
     ])
     Workout.find({})
-    .then(data =>{  
-        res.json(data)
-    })
-    .catch(err => { 
-        res.json(err)
-    })
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res.json(err)
+        })
 });
 
 //GET /api/workouts post a new workout to database
-router.post("/workouts", ({body}, res) => {
+router.post("/workouts", ({ body }, res) => {
     Workout.create(body)
         .then((dbWorkout => {
             res.json(dbWorkout);
         })).catch(err => {
             res.json(err);
         })
-      
-  });
 
-  
-  //   router.put("/workouts/:id", async function (req, res){
-      //       Workout.findOneAndUpdate(
-          //         { _id: req.params.id }
-          //   })
-          
-          router.get("/workouts/range", async function (req, res){})
-          
-          
-          
-          module.exports = router;
-          
-  
+});
+
+
+
+router.put("/workouts/:id", ({ body, params }, res) => {
+
+    Workout.findOneAndUpdate(
+        { _id: params.id },
+        {
+            $push: { exercises: body }
+        },
+        { new: true, runValidators: true }).then(dbWorkout => {
+            res.json(dbWorkout);
+        }).catch(err => {
+            res.json(err);
+        });
+
+});
+
+router.get("/workouts/range", async function (req, res) { 
+
+})
+
+
+
+module.exports = router;
+
